30bbfc2a6314b63b5ef0564e5604812746ea6417
Hector_Sancho <iocolopus@gmail.com>
2025-01-18 01:41:52 +0100

    revision hector

--- dependencias/Agentes/__init__.py
+++ dependencias/Agentes/__init__.py
@@ -1,6 +1,6 @@
 from dependencias.Agentes.Base_Agent import *
 from dependencias.Agentes.Chaser import *
-from dependencias.Agentes.Random_Angent import *
+from dependencias.Agentes.Random_Agent import *
 from dependencias.Agentes.Avoid_inmediate_death import *
 from dependencias.Agentes.Combined_Heuristic_Agent import *
 from dependencias.Agentes.Busqueda_anchura import *
